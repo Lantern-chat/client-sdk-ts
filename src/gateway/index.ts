bee98e0138fd205c6bac2820ea0f9806b24f7b16
novacrazy <novacrazy@gmail.com>
2022-02-22 02:30:56 -0600

    Improve gateway socket

--- src/gateway/index.ts
+++ src/gateway/index.ts
@@ -2,25 +2,46 @@ import WebSocket, { CloseEvent, ErrorEvent } from "isomorphic-ws";
 
 import type { ServerMsg, ClientMsg } from "../models";
 
-import { zlibSync as compress, unzlibSync as decompress } from 'fflate';
+import { zlibSync as compress, unzlibSync as decompress, FlateError } from 'fflate';
 import { MicroEmitter } from "../lib/emitter";
 
-export interface GatewayEventTypes {
-    'msg': (msg: ServerMsg) => void,
-    'error': (err: ErrorEvent) => void;
-    'close': (ev: CloseEvent) => void;
-    'open': () => void;
+export const enum GatewayErrorCode {
+    UnknownError = 4000,
+    UnknownOpcode = 4001,
+    DecodeError = 4002,
+    NotAuthenticated = 4003,
+    AuthFailed = 4004,
+
+    __MAX,
 }
+
+export const enum GatewayErrorKind {
+    Disconnected,
+    ServerError,
+    SocketError,
+    Compression,
+    Parse,
+    Unknown,
+}
+
+type GatewayErrorValue =
+    { t: GatewayErrorKind.Disconnected } |
+    { t: GatewayErrorKind.ServerError, e: GatewayErrorCode } |
+    { t: GatewayErrorKind.SocketError, e: ErrorEvent } |
+    { t: GatewayErrorKind.Compression, e: FlateError } |
+    { t: GatewayErrorKind.Parse, e: SyntaxError } |
+    { t: GatewayErrorKind.Unknown, e: any };
 
 export class GatewayError {
-    // TODO
-}
+    value: GatewayErrorValue;
 
-export const enum SocketState {
-    Closed,
-    Connecting,
-    Connected,
-    Errored,
+    constructor(value: GatewayErrorValue) {
+        this.value = value;
+    }
+
+    get kind(): GatewayErrorKind {
+        return this.value.t;
+    }
 }
 
 /**
@@ -28,70 +49,93 @@ export const enum SocketState {
  *
  * This does not handle heartbeats or identification.
  * */
-export class GatewaySocket extends MicroEmitter<GatewayEventTypes> {
+export class GatewaySocket extends MicroEmitter<{
+    msg(msg: ServerMsg): void;
+    error(err: GatewayError): void;
+    close(ev: CloseEvent): void;
+    open(): void;
+}> {
     private ws: WebSocket | null = null;
 
     private encoder: TextEncoder = new TextEncoder();
     private decoder: TextDecoder = new TextDecoder();
 
-    private s: SocketState = SocketState.Closed;
-
     /**
      * Connects to the WebSocket, resolving the promise on open or rejecting on error.
      *
      * @param uri Websocket URI
-     * @returns Promise<void, ErrorEvent>
+     * @returns Promise<void, GatewayError>
      */
     connect(uri: string): Promise<void> {
-        this.s = SocketState.Connecting;
         let ws = this.ws = new WebSocket(uri);
         ws.binaryType = 'arraybuffer';
 
-        ws.addEventListener('open', () => {
-            this.s = SocketState.Connected;
-            this.emit('open');
-        });
+        ws.addEventListener('open', () => this.emit('open'));
 
         ws.addEventListener('message', msg => {
-            // decompress, decode, parse, emit
-            this.emit('msg', JSON.parse(this.decoder.decode(decompress(new Uint8Array(msg.data as ArrayBuffer)))));
+            try {
+                // decompress, decode, parse, emit
+                this.emit('msg', JSON.parse(this.decoder.decode(decompress(new Uint8Array(msg.data as ArrayBuffer)))));
+            } catch(e) {
+                this.emit('error', error(e));
+            }
         });
 
         ws.addEventListener('close', ev => {
             this.ws = null; // ensure closed state
-            this.s = SocketState.Closed;
+            let code = ev.code;
+            if(code >= 4000 && code < GatewayErrorCode.__MAX) {
+                this.emit('error', new GatewayError({ t: GatewayErrorKind.ServerError, e: code }));
+            }
             this.emit('close', ev);
         });
 
-        ws.addEventListener('error', err => {
-            this.ws = null; // ensure closed state
-            this.s = SocketState.Errored;
-            this.emit('error', err);
-        });
+        ws.addEventListener('error', e => this.emit('error', new GatewayError({ t: GatewayErrorKind.SocketError, e })));
 
         return new Promise((resolve, reject) => {
             let resolve_open = this.once('open', () => {
                 resolve();
                 this.off('error', resolve_err); // cleanup the other
-            }), resolve_err = this.once('error', (err: ErrorEvent) => {
+            }), resolve_err = this.once('error', (err: GatewayError) => {
                 reject(err);
                 this.off('open', resolve_open); // cleanup the other
             });
         });
     }
 
-    close() { this.ws?.close(); }
+    close() { this.ws?.close() }
 
+    /**
+     *
+     * @param msg
+     * @throws GatewayError
+     */
     send(msg: ClientMsg) {
-        if(!this.ws) {
-            throw new GatewayError();
+        // if exists and open
+        if(this.readyState === 1) {
+            try {
+                // serialize, encode, compress, send
+                this.ws!.send(compress(this.encoder.encode(JSON.stringify(msg)), { level: 9 }))
+            } catch(e) {
+                throw error(e);
+            }
+
+        } else {
+            throw new GatewayError({ t: GatewayErrorKind.Disconnected });
         }
-
-        // serialize, encode, compress, send
-        this.ws.send(compress(this.encoder.encode(JSON.stringify(msg)), { level: 9 }))
     }
 
-    get state(): SocketState {
-        return this.s;
+    get readyState(): undefined | typeof WebSocket.CONNECTING | typeof WebSocket.OPEN | typeof WebSocket.CLOSING | typeof WebSocket.CLOSED {
+        return this.ws?.readyState;
     }
+}
+
+function error(e: any): GatewayError {
+    let kind = GatewayErrorKind.Unknown;
+    if(e instanceof SyntaxError) {
+        kind = GatewayErrorKind.Parse;
+    } else if(e instanceof Error) {
+        kind = GatewayErrorKind.Compression;
+    }
+    return new GatewayError({ t: kind, e });
 }
