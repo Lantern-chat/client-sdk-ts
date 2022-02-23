58411f5dd060b00d8a6798289bf734c9c88fd725
novacrazy <novacrazy@gmail.com>
2022-02-23 06:35:38 -0600

    Fix party API paths

--- src/api/commands/party.ts
+++ src/api/commands/party.ts
@@ -6,9 +6,9 @@ export const GetParty = /*#__PURE__*/command<{ party_id: Snowflake }, Party>({
 });
 
 export const GetPartyMembers = /*#__PURE__*/command<{ party_id: Snowflake }, Array<PartyMember>>({
-    path() { return `/party/${this.party_id}`; }
+    path() { return `/party/${this.party_id}/members`; }
 });
 
 export const GetPartyRooms = /*#__PURE__*/command<{ party_id: Snowflake }, Array<Room>>({
-    path() { return `/party/${this.party_id}`; }
+    path() { return `/party/${this.party_id}/rooms`; }
 });
