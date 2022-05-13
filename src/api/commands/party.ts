3d6c1d64cf35c7d36085cd445a6a0d126452809a
novacrazy <novacrazy@gmail.com>
2022-05-13 16:18:16 -0500

    Add GetPartyInvites and CreatePartyInvite commands

--- src/api/commands/party.ts
+++ src/api/commands/party.ts
@@ -1,5 +1,5 @@
 import { command } from "../command";
-import type { Party, PartyMember, Room, Snowflake } from "../../models";
+import type { Party, PartyMember, Room, Snowflake, Invite } from "../../models";
 
 export const GetParty = /*#__PURE__*/command<{ party_id: Snowflake }, Party>({
     path() { return `/party/${this.party_id}`; }
@@ -11,4 +11,18 @@ export const GetPartyMembers = /*#__PURE__*/command<{ party_id: Snowflake }, Arr
 
 export const GetPartyRooms = /*#__PURE__*/command<{ party_id: Snowflake }, Array<Room>>({
     path() { return `/party/${this.party_id}/rooms`; }
+});
+
+export const GetPartyInvites = /*#__PURE__*/command<{ party_id: Snowflake }, Array<Invite>>({
+    path() { return `/party/${this.party_id}/invites`; }
+});
+
+export interface CreatePartyInviteForm {
+    max_uses?: number,
+    duration?: number,
+    description?: string,
+}
+
+export const CreatePartyInvite = /*#__PURE__*/command.post<{ party_id: Snowflake }, Invite, CreatePartyInviteForm>({
+    path() { return `/party/${this.party_id}/invites`; }
 });
